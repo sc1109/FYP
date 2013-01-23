68160fc12265414d3048d33834251861faa9120d
Sam Colliss <swcolliss@gmail.com>
2013-01-23 11:04:39 +0000

    testRot fixed

--- testRot.m
+++ testRot.m
@@ -6,18 +6,18 @@ R(1,2) = c/sqrt(c^2+f^2);
 R(2,1) = -R(1,2);
 R(2,2) = R(1,1);
 
-xyz = [c 40 30 20 10;f f f f f];
+xyz = [-c -40 -30 -20 -10;f f f f f];
 K = R*xyz;
 
-plot(c,50,'+');
+plot(-c,50,'+');
 hold on;
-a = plot(40,50,'+');
+a = plot(-40,50,'+');
 set(a,'Color','red');
-a = plot(30,50,'+');
+a = plot(-30,50,'+');
 set(a,'Color','yellow');
-a = plot(20,50,'+');
+a = plot(-20,50,'+');
 set(a,'Color','green');
-a = plot(10,50,'+');
+a = plot(-10,50,'+');
 set(a,'Color','magenta');
 plot(K(1,1),50,'*');
 a = plot(K(1,2),50,'*');
