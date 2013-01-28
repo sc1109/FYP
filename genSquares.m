b817cbb90dada40f524299970686b6508f15fc89
Sam Colliss <swcolliss@gmail.com>
2013-01-28 15:33:43 +0000

    time to sync

--- genSquares.m
+++ genSquares.m
@@ -1,56 +1,65 @@
 function [ u, v ] = genSquares( xyz, t, f )
 
-figure;
+a = figure;
 subplot(3,2,1);
 [X,Y] = meshgrid(xyz(1,:),xyz(2,:));
 Z = meshgrid(xyz(3,:));
-surf(X,Y,Z);
+surf(X,Z,Y);
+axis([0 300 0 300 0 300]);
+daspect([1 1 1]);
+
+b = figure;
+subplot(3,2,1);
+xyzsq = cat(2,xyz(:,1:2),[max(xyz(1,:)) max(xyz(2,:)) max(xyz(3,:))]', xyz(:,3));
+patch(xyzsq(1,:),xyzsq(2,:),'blue');
+axis([0 300 0 300]);
+daspect([1 1 1]);
 
 u = zeros(6,3);
 v = zeros(6,3);
 w = zeros(6,3);
 u(1,:) = xyz(1,:) - 150;
 v(1,:) = xyz(2,:) - 150;
-z = xyz(3,:);
+w(:,:) = cat(1,xyz(3,:),xyz(3,:),xyz(3,:),xyz(3,:),xyz(3,:),xyz(3,:));
 %w(1,:) = xyz(3,:) - 150;
 
 for n = 1:5
-    u(n+1,1) = floor(u(n,1) + (f*u(1,1))/(z(1)-n*t));
+    u(n+1,1) = floor(u(n,1) + (f*u(1,1))/(w(1,1)-n*t));
     if(u(n+1,1)<-149)
         u(n+1,1)=-149;
     end
     if(u(n+1,1)>150)
         u(n+1,1)=150;
     end
-    v(n+1,1) = floor(v(n,1) + (f*v(1,1))/(z(1)-n*t));
+    v(n+1,1) = floor(v(n,1) + (f*v(1,1))/(w(1,1)-n*t));
     if(v(n+1,1)<-149)
         v(n+1,1)=-149;
     end
     if(v(n+1,1)>150)
         v(n+1,1)=150;
     end
-    u(n+1,2) = floor(u(n,2) + (f*u(1,2))/(z(2)-n*t));
+    u(n+1,2) = floor(u(n,2) + (f*u(1,2))/(w(1,2)-n*t));
     if(u(n+1,2)<-149)
         u(n+1,2)=-149;
     end
     if(u(n+1,2)>150)
         u(n+1,2)=150;
     end
-    v(n+1,2) = floor(v(n,2) + (f*v(1,2))/(z(2)-n*t));
+    v(n+1,2) = floor(v(n,2) + (f*v(1,2))/(w(1,2)-n*t));
     if(v(n+1,2)<-149)
         v(n+1,2)=-149;
     end
     if(v(n+1,2)>150)
         v(n+1,2)=150;
     end
-    u(n+1,3) = floor(u(n,3) + (f*u(1,3))/(z(3)-n*t));
+    u(n+1,3) = floor(u(n,3) + (f*u(1,3))/(w(1,3)-n*t));
     if(u(n+1,3)<-149)
         u(n+1,3)=-149;
     end
     if(u(n+1,3)>150)
         u(n+1,3)=150;
     end
-    v(n+1,3) = floor(v(n,3) + (f*v(1,3))/(z(3)-n*t));
+    v(n+1,3) = floor(v(n,3) + (f*v(1,3))/(w(1,3)-n*t));
     if(v(n+1,3)<-149)
         v(n+1,3)=-149;
     end
@@ -58,7 +67,7 @@ for n = 1:5
         v(n+1,3)=150;
     end
     
-    if (Z<(n+1)*t)
+    if (w(1,1)<(n+1)*t && w(1,2)<(n+1)*t && w(1,3)<(n+1)*t)
         u(n+1,:)=-149;
         v(n+1,:)=-149;
     end
@@ -73,22 +82,71 @@ uvw_part(:,:,n) = cat(1,u(n,:),v(n,:),w(n,:));
 end
 uvw = cat(3,uvw_part(:,:,1:6));
 
+figure(a);
 subplot(3,2,2);
-plot3(u(2,1):u(2,2), v(2,1):v(2,3), Z);
-[X,Y] = meshgrid(uvw(1,:),uvw(2,:));
-Z = meshgrid(uvw(3,:));
-surf(X,Y,Z);
+[X,Y] = meshgrid(uvw(1,:,2),uvw(2,:,2));
+Z = meshgrid(uvw(3,:,2));
+surf(X,Z,Y);
+axis([0 300 0 300 0 300]);
+daspect([1 1 1]);
 
 subplot(3,2,3);
-plot3(u(3,1):u(3,2), v(3,1):v(3,3), Z);
+[X,Y] = meshgrid(uvw(1,:,3),uvw(2,:,3));
+Z = meshgrid(uvw(3,:,3));
+surf(X,Z,Y);
+axis([0 300 0 300 0 300]);
+daspect([1 1 1]);
 
 subplot(3,2,4);
-plot3(u(4,1):u(4,2), v(4,1):v(4,3), Z);
+[X,Y] = meshgrid(uvw(1,:,4),uvw(2,:,4));
+Z = meshgrid(uvw(3,:,4));
+surf(X,Z,Y);
+axis([0 300 0 300 0 300]);
+daspect([1 1 1]);
 
 subplot(3,2,5);
-plot3(u(5,1):u(5,2), v(5,1):v(5,3), Z);
+[X,Y] = meshgrid(uvw(1,:,5),uvw(2,:,5));
+Z = meshgrid(uvw(3,:,5));
+surf(X,Z,Y);
+axis([0 300 0 300 0 300]);
+daspect([1 1 1]);
 
 subplot(3,2,6);
-plot3(u(6,1):u(6,2), v(6,1):v(6,3), Z);
+[X,Y] = meshgrid(uvw(1,:,6),uvw(2,:,6));
+Z = meshgrid(uvw(3,:,6));
+surf(X,Z,Y);
+axis([0 300 0 300 0 300]);
+daspect([1 1 1]);
+
+figure(b);
+subplot(3,2,2);
+uvwsq = cat(2,uvw(:,1:2,2),[max(uvw(1,:,2)) max(uvw(2,:,2)) max(uvw(3,:,2))]', uvw(:,3,2));
+patch(uvwsq(1,:),uvwsq(2,:),'blue');
+axis([0 300 0 300]);
+daspect([1 1 1]);
+
+subplot(3,2,3);
+uvwsq = cat(2,uvw(:,1:2,3),[max(uvw(1,:,3)) max(uvw(2,:,3)) max(uvw(3,:,3))]', uvw(:,3,3));
+patch(uvwsq(1,:),uvwsq(2,:),'blue');
+axis([0 300 0 300]);
+daspect([1 1 1]);
+
+subplot(3,2,4);
+uvwsq = cat(2,uvw(:,1:2,4),[max(uvw(1,:,4)) max(uvw(2,:,4)) max(uvw(3,:,4))]', uvw(:,3,4));
+patch(uvwsq(1,:),uvwsq(2,:),'blue');
+axis([0 300 0 300]);
+daspect([1 1 1]);
+
+subplot(3,2,5);
+uvwsq = cat(2,uvw(:,1:2,5),[max(uvw(1,:,5)) max(uvw(2,:,5)) max(uvw(3,:,5))]', uvw(:,3,5));
+patch(uvwsq(1,:),uvwsq(2,:),'blue');
+axis([0 300 0 300]);
+daspect([1 1 1]);
+
+subplot(3,2,6);
+uvwsq = cat(2,uvw(:,1:2,6),[max(uvw(1,:,6)) max(uvw(2,:,6)) max(uvw(3,:,6))]', uvw(:,3,6));
+patch(uvwsq(1,:),uvwsq(2,:),'blue');
+axis([0 300 0 300]);
+daspect([1 1 1]);
 
 end
--- testRotSq.m
+++ testRotSq.m
@@ -1,24 +1,27 @@
 clear all;
-c = 50;
+c = 5;
 f = 30;
 R(1,1) = f/sqrt(c^2+f^2);
 R(1,2) = c/sqrt(c^2+f^2);
 R(2,1) = -R(1,2);
 R(2,2) = R(1,1);
 
-z = floor((140*R(1,1))/R(1,2));
-xyz = [-140 -130 -120 -110 -100;z z z z z];
-K = floor(R*xyz);
+z = floor(10*f/c);
+xyz = [10 20 30 40 50;z z z z z];
+K = round(R*xyz);
 
-figure
+a = figure;
 subplot(2,1,1);
-a = plot(linspace(-140,-100,5),50,'*');
-axis([-145 25 49 51]);
-%set(a,'Color','blue');
+plot(xyz(1,:),xyz(2,:),'*');
+%axis([-50 100 -200 100]);
+daspect([1 1 1]);
 subplot(2,1,2);
-a = plot(K(1,:),50,'*');
-axis([-145 25 49 51]);
-%set(a,'Color','red');
+plot(K(1,:),K(2,:),'*');
+%axis([-50 100 -200 100]);
+daspect([1 1 1]);
 
-genSquares(linspace(c+150,140,5),linspace(100,140,5),[z z z],5,30);
-genSquares(K(1,:)+150,linspace(100,140,5),[K(2,1),K(2,5),K(2,1)],5,30);
+
+%xyz = [140 100 z; c+150 100 z; 140 140 z]';
+%genSquares(xyz,5,30);
+%xyz = [K(1,1) 100 K(2,1); K(1,5) 100 K(2,5); K(1,1) 140 K(2,1)]';
+%genSquares(xyz,5,30);
